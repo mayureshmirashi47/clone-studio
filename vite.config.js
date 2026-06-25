b53a685c5318af97a62bbfd05ee883ab6dae1658
Mayuresh Mirashi <143743589+mayureshmirashi47@users.noreply.github.com>
2026-06-25 13:03:08 +0530

    Fix import statement for react plugin in config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react ribbons from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins: [react()],
-base: '/wix-studio-clone/',
+  plugins: [react()],
+  base: '/wix-studio-clone/', 
 })
